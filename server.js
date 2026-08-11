1fb14ffaab50e1ab6e9951941c997abfe68f833a
Michael Taye <michaeltaye200@gmail.com>
2026-08-11 16:19:04 +0300

    Update

--- server.js
+++ server.js
@@ -39,7 +39,10 @@ app.get("/taxiapp", (req, res) => {
   res.sendFile(path.join(__dirname, "taxiapp.html"));
 });
 
-app.use(express.static(path.join(__dirname, "dist")));
+app.use('/src/assets', express.static(path.join(__dirname, 'src', 'assets')));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'src', 'assets')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.use((req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
